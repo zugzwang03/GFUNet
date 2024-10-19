55b7e213dccdcd9c0fffb816a973694d02b76caf
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2024-10-19 10:08:58 +0530

    changed formatting

--- test.py
+++ test.py
@@ -49,12 +49,8 @@ for output_mask in output_masks:
     idx += 1
     threshold = np.mean(output_mask[0])  # Set a threshold value to binarize the output
     output_mask = (output_mask > threshold).astype(np.float32)
-    output_image = Image.fromarray(
-        (output_mask[0] * 255).astype("uint8")
-    )  # Convert mask to image
+    output_image = Image.fromarray((output_mask[0] * 255).astype("uint8"))  # Convert mask to image
     output_image = output_image.resize((imageHeight, imageWidth))
-    save_path = os.path.join(
-        "/content/drive/MyDrive/Fusion", leftOrRight, f"pred_{idx}.png"
-    )  # Update the path to save the mask
+    save_path = os.path.join("/content/drive/MyDrive/Fusion", leftOrRight, f"pred_{idx}.png")  # Update the path to save the mask
     output_image.save(save_path)
     print(f"Output mask saved at {save_path}")
